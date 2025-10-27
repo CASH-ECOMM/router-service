d1a19a48a4d213ef2c6bb81d38c61acbf2123c8a
Dave <dave@mac.home>
2025-10-27 12:29:00 -0400

    integrate CatalogueService gRPC with RouterService

---
+++ src/main/java/com/cash/services/CatalogueService.java
@@ -0,0 +1,31 @@
+package com.cash.services;
+
+import com.cash.grpc.catalogue.*;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CatalogueService {
+
+    private final CatalogueServiceGrpc.CatalogueServiceBlockingStub blockingStub;
+
+    public CatalogueService() {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+        blockingStub = CatalogueServiceGrpc.newBlockingStub(channel);
+    }
+
+    public ItemList getAllItems() {
+        return blockingStub.getAllItems(Empty.newBuilder().build());
+    }
+
+    public ItemList searchItems(String keyword) {
+        return blockingStub.searchItems(SearchRequest.newBuilder().setKeyword(keyword).build());
+    }
+
+    public ItemResponse createItem(CreateItemRequest request) {
+        return blockingStub.createItem(request);
+    }
+}
